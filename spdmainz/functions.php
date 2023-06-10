7fb84ac08c2bc3a44bf140ae2df71413ca46916f
Erik <erik.donner@posteo.de>
2023-06-10 11:21:25 +0200

    update theme

--- spdmainz/functions.php
+++ spdmainz/functions.php
@@ -1,6 +1,9 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-    wp_enqueue_style( 'child-style', get_template_directory_uri().'/style.css' );
-    wp_enqueue_style( 'child-stylescss', get_template_directory_uri().'/css/style.css' );
+/**
+ * Enabling Child Theme
+ */
+add_action( 'wp_enqueue_scripts', 'child_theme_styles', PHP_INT_MAX );
+function child_theme_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() .'/css/style.css' , array('parent-style'));
 }
