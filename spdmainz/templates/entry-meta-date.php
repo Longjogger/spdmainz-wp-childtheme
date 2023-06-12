6761d12178f6edc737d1fb628966791453e330f8
Erik <erik.donner@posteo.de>
2023-06-12 16:11:49 +0200

    move date to subhead

--- spdmainz/templates/entry-meta-date.php
+++ spdmainz/templates/entry-meta-date.php
@@ -5,11 +5,11 @@
  * @package Virtue Theme
  */
 
-if ( ( 'post' === get_post_type() && '1' === virtue_premium_get_option( 'hide_postdate' ) ) || ( 'post' !== get_post_type() && '1' === virtue_premium_get_option( 'custom_post_show_postdate' ) ) ) {
+/* if ( ( 'post' === get_post_type() && '1' === virtue_premium_get_option( 'hide_postdate' ) ) || ( 'post' !== get_post_type() && '1' === virtue_premium_get_option( 'custom_post_show_postdate' ) ) ) {
 	?>
 	<div>
         <meta itemprop="datePublished" content="<?php echo esc_attr( get_the_modified_date( 'c' ) ); ?>">
 		<?php echo esc_html( get_the_date( 'j. F Y' ) ); ?>
 	</div>
 	<?php
-}
+} */
