3ba757db02a4418a5bd0c08e8e56249d683610e7
Erik <erik.donner@posteo.de>
2023-06-12 15:43:42 +0200

    corr month

--- spdmainz/templates/entry-meta-date.php
+++ spdmainz/templates/entry-meta-date.php
@@ -9,7 +9,7 @@ if ( ( 'post' === get_post_type() && '1' === virtue_premium_get_option( 'hide_po
 	?>
 	<div>
         <meta itemprop="datePublished" content="<?php echo esc_attr( get_the_modified_date( 'c' ) ); ?>">
-		<?php echo esc_html( get_the_date( 'j. m Y' ) ); ?>
+		<?php echo esc_html( get_the_date( 'j. F Y' ) ); ?>
 	</div>
 	<?php
 }
