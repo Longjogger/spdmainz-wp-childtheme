40e4e2e7ab9ef400841593c15f4ca4d2c0641957
Erik <erik.donner@posteo.de>
2023-06-12 07:41:10 +0200

    disable author page

--- spdmainz/functions.php
+++ spdmainz/functions.php
@@ -7,3 +7,13 @@ function child_theme_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() .'/css/style.css' , array('parent-style'));
 }
+
+/**
+ * Disabling Author Page
+ */
+function rn_author_page_redirect() {
+    if ( is_author() ) {
+        wp_redirect( home_url() );
+    }
+}
+add_action( 'template_redirect', 'rn_author_page_redirect' );
