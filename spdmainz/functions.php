6ac2cd1a8a40b210f47c39dcdab619db669d820d
Erik <erik.donner@posteo.de>
2023-06-14 16:40:04 +0200

    corr

--- spdmainz/functions.php
+++ spdmainz/functions.php
@@ -9,7 +9,7 @@ function child_theme_styles() {
 add_action( 'wp_enqueue_scripts', 'child_theme_styles', PHP_INT_MAX );
 
 // Include if Team Showcase is used
-$current_page_id = get_the_ID();
+$current_page_id = get_queried_object_id();
 // Get the content of the current page
 $content = get_post_field('post_content', $current_page_id);
 
